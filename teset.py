89c16706c212f6bfbcbc27db24516fe68183ecfe
Shawn Swist <sswisty7@gmail.com>
2017-04-21 16:24:41 -0400

    Xbox controller with/without xboxdrv running

--- teset.py
+++ teset.py
@@ -6,6 +6,11 @@ If xboxdrv is not running the axes for the controller are off
     The right thumb y axis is read as the right trigger, noticed this before 
     with ROV-Siren code. Changes in that repository should be sufficient to 
     work here. If xboxdrv is running the controller is read properly.
+    
+    Ltrigger -> Rthumb x
+    Rthumb x -> Rtrigger
+    Rthumb y -> Rthumb x
+    Rtrigger -> Ltrigger
 
 Created on Fri Apr 21 09:58:01 2017
 
@@ -34,8 +39,13 @@ XboxCont.start()
 while Operational:
     
     try:
-        print "RTrigger {}, LThumb y {}".format(XboxCont.RTRIGGER, XboxCont.LTHUMBY)
-        
+        print "RTrigger {}, LTrigger {}".format(round(XboxCont.RTRIGGER), round(XboxCont.LTRIGGER))
+       # print "LThumb x {}, LThumb y {}".format(round(XboxCont.LTHUMBX), round(XboxCont.LTHUMBY))
+       # print "RThumb x {}, RThumb y {}".format(round(XboxCont.RTHUMBX), round(XboxCont.RTHUMBY))
+       # print " "
+       # print " "
+       # print " "
+       
     except KeyboardInterrupt:
         XboxCont.stop()
         print "User Cancled"
